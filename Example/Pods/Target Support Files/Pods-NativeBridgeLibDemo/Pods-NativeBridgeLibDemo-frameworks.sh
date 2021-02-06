69862604d889f6c04599377d1c830536abb1bebd
Mariko <18502702013@163.com>
2021-02-06 11:36:14 +0800

    feat: 增加网络库

--- Example/Pods/Target Support Files/Pods-NativeBridgeLibDemo/Pods-NativeBridgeLibDemo-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-NativeBridgeLibDemo/Pods-NativeBridgeLibDemo-frameworks.sh
@@ -175,12 +175,14 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NativeBridgeLib/NativeBridgeLib.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SSZipArchive/SSZipArchive.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WebViewJavascriptBridge/WebViewJavascriptBridge.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYCache/YYCache.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NativeBridgeLib/NativeBridgeLib.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SSZipArchive/SSZipArchive.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WebViewJavascriptBridge/WebViewJavascriptBridge.framework"
